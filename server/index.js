c4d7a334b0b79eb6b7f6ac0728e62b29a22a9cfb
sunyishen <43066736+sunyishen@users.noreply.github.com>
2022-05-18 22:15:05 -0500

    init commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// route handler, handle get request from chrome/user
+app.get('/', (req, res) => {
+    res.send({hi:'there'});
+});
+// get a port from https://www.heroku.com/
+// if not defined env port, we use 50000
+const PORT = process.env.PORT || 50000;
+app.listen(PORT);
