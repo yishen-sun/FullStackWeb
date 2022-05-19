24bb10458d88e541497f8727070b7f1f9b641778
yishen.sun <ethan.ys.sun@gmail.com>
2022-05-18 22:58:57 -0500

    PY

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // route handler, handle get request from chrome/user
 app.get('/', (req, res) => {
-    res.send({hi:'there'});
+    res.send({Hi:'PY, I love you!'});
 });
 // get a port from https://www.heroku.com/
 // if not defined env port, we use 50000
