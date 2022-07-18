70016939e70d29cdefcb3c0b093bbf3f904ab0d7
yishen.sun <ethan.ys.sun@gmail.com>
2022-07-18 00:07:23 -0500

    final polish

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -25,7 +25,7 @@ class Header extends Component {
             <nav>
             <div className="nav-wrapper">
 
-              <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Emaily</Link>
+              <Link to={this.props.auth ? '/' : '/'} className="left brand-logo">Emaily</Link>
               <ul id="nav-mobile" className="right">
                 {this.renderContent()}
               </ul>
